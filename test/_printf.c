a2d95f5a8fc830995a3eb57e88cfc375a7e2cea8
alejandrodgz <5199@holbertonschool.com>
2022-07-20 16:17:05 -0500

    💪 the test directory creation with all the needed files

---
+++ test/_printf.c
@@ -0,0 +1,78 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * check_specifiers - checks if there is a valid format specifier
+ * @format: possible format specifier
+ *
+ * Return: pointer to valid function or NULL
+ */
+int (*check_specifiers(const char *format))(va_list)
+{
+	unsigned int a;
+	print_t ar[] = {
+	{"c", print_c},
+	{"s", print_s},
+	{NULL, NULL}
+	};
+
+	for (a = 0; ar[a].a != NULL; a++)
+	{
+		if (*(ar[a].a) == *format)
+		{
+			break;
+		}
+	}
+	return (ar[a].f);
+}
+
+/**
+ * _printf - prints the chars
+ * @format: list of arguments
+ *
+ * Return: chars printed
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int a = 0, b = 0;
+	va_list list_spe;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+	va_start(list_spe, format);
+	while (format[a])
+	{
+		for (; format[a] != '%' && format[a]; a++)
+		{
+			_putchar(format[a]);
+			b++;
+		}
+
+		if (!format[a])
+		{
+			return (b);
+		}
+
+		f = check_specifiers(&format[a + 1]);
+		if (f != NULL)
+		{
+			b += f(list_spe);
+			a += 2;
+			continue;
+		}
+
+		if (!format[a + 1])
+			return (-1);
+		_putchar(format[a]);
+		b++;
+		if (format[a + 1] == '%')
+			a += 2;
+		else
+			a++;
+	}
+	va_end(list_spe);
+	return (b);
+}
---
+++ test/fun.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_c - prints char format
+ * @c: character
+ * Return: return value to counter
+ */
+int print_c(va_list c)
+{
+	char c_h;
+
+	c_h = (char)va_arg(c, int);
+	_putchar(c_h);
+	return (1);
+}
+
+/**
+ * print_s - prints string format
+ * @s: string
+ * Return: return value to counter
+ */
+
+int print_s(va_list s)
+{
+	unsigned int j;
+	char *po;
+
+	po = va_arg(s, char *);
+	if (po == NULL)
+		po = "(nil)";
+	for (j = 0; po[j] != '\0'; j++)
+	{
+		_putchar(po[j]);
+	}
+	return (j);
+}
