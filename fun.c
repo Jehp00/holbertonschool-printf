bf656923935ef266c5f208ddf36afccd0dc5385e
alejandrodgz <5199@holbertonschool.com>
2022-07-20 12:12:13 -0500

    📝, fun.c file creation

--- fun.c
+++ fun.c
@@ -2,3 +2,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int print_c(va_list c)
+{
+	char c_h;
+	c_h = (char)va_arg(c, int);
+	_putchar(c_h);
+	return (1);
+}
+
+int print_s(va_list s)
+{
+	unsigned int j;
+	char *po;
+	po = va_arg(s, char *);
+	if (po = NULL)
+		po = "(nil)";
+	for (j = 0; po[j] != '\0'; j++)
+	{
+		_putchar(po[j]);
+	}
+	return (j);
+}
+
+int print_%(va_list %)
+{
+	putchar ('%');
+	return (1);
+}
