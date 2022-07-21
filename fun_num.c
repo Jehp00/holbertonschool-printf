e04479f19f27d21456d15889005823e4c42a1006
Jehp00 <Jehp01104006@gmail.com>
2022-07-21 15:50:40 -0500

    💟fun_num int

--- fun_num.c
+++ fun_num.c
@@ -8,28 +8,35 @@
  */
 int print_i(va_list i)
 {
+	unsigned int deci, digi, natu;
 	int n = va_arg(i, int);
-	int d, len;
-	unsigned int num;
-
-	d = 1;
-	len = 0;
-	num = n;
-	if (n < 0)
+	int count = 0;
+	double f = 1;
+	if (n == 0)
 	{
-		putchar('-');
-		len++;
-		num = -n;
+		_putchar('0');
+		return (1);
 	}
-
-	while (num / d > 9)
-		d *= 10;
-	while (d != 0)
+	else
 	{
-		putchar('0' + num / d);
-		len++;
-		num %= d;
-		d /= 10;
+		if (n < 0)
+		{
+			_putchar('-');
+			natu = -n;
+			count++;
+		}
+		else
+			natu = n;
+		while (f <= natu)
+			f = f * 10;		
+		deci = f / 10;
+		while (deci >= 1)
+		{
+			digi = natu / deci;
+			_putchar(digi + '0'), count++;
+			natu = (natu - (deci * digi));
+			deci = deci / 10;
+		}
 	}
-	return (len);
+	return (count);
 }
