f4d61216e44c89d128c02985bb98c5d40b631785
alejandrodgz <5199@holbertonschool.com>
2022-07-22 15:42:03 -0500

    🤏🏽, HEX and hex specifiers added

--- main.h
+++ main.h
@@ -11,8 +11,10 @@ int _printf(const char *format, ...);
 int print_c(va_list c);
 int print_s(va_list s);
 int print_i(va_list i);
-int print_b(va_list b);
 int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+
 int (*check_specifiers(const char *format))(va_list);
 
 /*Structures*/
