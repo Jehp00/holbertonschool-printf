075802b59ab3816b57ecea21640f5811342398f3
alejandrodgz <5199@holbertonschool.com>
2022-07-22 18:56:30 -0500

    😁, first attempt fun_S

--- main.h
+++ main.h
@@ -12,9 +12,9 @@ int print_c(va_list c);
 int print_s(va_list s);
 int print_i(va_list i);
 int print_o(va_list o);
-int print_u(va_list u);
 int print_x(va_list x);
 int print_X(va_list X);
+int print_S(va_list S);
 
 int (*check_specifiers(const char *format))(va_list);
 
