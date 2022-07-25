4bd9626709c3f22e472cd539b65e7d1de838cbf8
Jehp00 <Jehp01104006@gmail.com>
2022-07-25 12:25:17 -0500

    fun_S string with condiction ASCII

--- fun_S.c
+++ fun_S.c
@@ -3,6 +3,30 @@
 #include <stdio.h>
 
 /**
+ * hex_print - prints a char's ascii value in uppercase hex
+ * @c: char to print
+ *
+ * Return: number of chars printed (always 2)
+ */
+int hex_print(char c)
+{
+	int count;
+	char diff = 'A' - ':';
+	char d[2];
+
+	d[0] = c / 16;
+	d[1] = c % 16;
+	for (count = 0; count < 2; count++)
+	{
+		if (d[count] >= 10)
+			_putchar('0' + diff + d[count]);
+		else
+			_putchar('0' + d[count]);
+	}
+	return (count);
+}
+
+/**
  * print_S - prints string format
  * @S: string
  * Return: return value to counter
@@ -25,7 +49,7 @@ int print_S(va_list S)
 			_putchar('\\');
 			_putchar('x');
 			quot += 2;
-			quot += print_X(string[a]);
+			quot += hex_print(string[a]);
 		}
 		else
 		{
